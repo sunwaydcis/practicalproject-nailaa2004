96c789609b514b69e158530b82ae2ecd7bdbc72c
naila raja <nailaraja985@gmail.com>
2025-07-23 17:15:09 +0500

    yas

---
+++ src/main/scala-3/ch/makery/address/Main.App.scala
@@ -0,0 +1,63 @@
+package ch.makery.address
+
+import javafx.fxml.FXMLLoader
+import scalafx.application.JFXApp3
+import scalafx.application.JFXApp3.PrimaryStage
+import scalafx.scene.Scene
+import scalafx.Includes.*
+import javafx.scene as jfxs
+import scalafx.collections.ObservableBuffer
+import ch.makery.address.model.Person
+
+
+object MainApp extends JFXApp3:
+
+  //Window Root Pane
+  var roots: Option[scalafx.scene.layout.BorderPane] = None
+
+  override def start(): Unit =
+    // transform path of RootLayout.fxml to URI for resource location.
+    val rootResource = getClass.getResource("/ch/makery/address/view/RootLayout.fxml")
+    // initialize the loader object.
+    val loader = new FXMLLoader(rootResource)
+    // Load root layout from fxml file.
+    loader.load()
+
+    // retrieve the root component BorderPane from the FXML
+    roots = Option(loader.getRoot[jfxs.layout.BorderPane])
+
+    stage = new PrimaryStage():
+      title = "AddressApp"
+      scene = new Scene():
+        root = roots.get
+
+    // call to display PersonOverview when app start
+    showPersonOverview()
+  // actions for display person overview window
+  def showPersonOverview(): Unit =
+    val resource = getClass.getResource("view/PersonOverview.fxml")
+    val loader = new FXMLLoader(resource)
+    loader.load()
+    val roots = loader.getRoot[jfxs.layout.AnchorPane]
+    this.roots.get.center = roots
+
+    /**
+     * The data as an observable list of Persons.
+     */
+    val personData = new ObservableBuffer[Person]()
+
+    /**
+     * Constructor
+     */
+    personData += new Person("Hans", "Muster")
+    personData += new Person("Ruth", "Mueller")
+    personData += new Person("Heinz", "Kurz")
+    personData += new Person("Cornelia", "Meier")
+    personData += new Person("Werner", "Meyer")
+    personData += new Person("Lydia", "Kunz")
+    personData += new Person("Anna", "Best")
+    personData += new Person("Stefan", "Meier")
+    personData += new Person("Martin", "Mueller")
+
+
+
